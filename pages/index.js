2b3dee7d9ca81dcf757ea1b4816fee51aa166f1d
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-04-19 11:01:56 -0300

    Update index querys DB with getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { PrismaClient } from '@prisma/client'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({categorias}) {
+  
+  console.log(categorias);
   return(
     <div className='bg-red-600 '>
+
+      
       <h1>Next.js</h1>
 
     </div>
   ) 
+}
+
+export const getServerSideProps = async () =>{
+  const prisma = new PrismaClient();
+  
+  const categorias = await prisma.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    }
+  }
 }
